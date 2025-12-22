f9e8d457cd078c15a1fd1b294150516afb303723
ThibautDona <donathibaut02@gmail.com>
2025-12-22 03:19:56 +0100

    Layout OK + Responsive OK + english comments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,27 +24,29 @@ export default function Home() {
                     <h1>Bonjour, je suis John Doe</h1>
                     <h2>Développeur web full stack</h2>
 
-                    {/* Modale Bootstrap */}
+                    {/* Bootstrap Modal */}
                     <button className="head__modal-button btn btn-danger" type="button" data-bs-toggle="modal" data-bs-target="#head__modal">
                         En savoir plus
                     </button>
                 </div>
-                <div className="head__modal modal fade" id="head__modal" tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-centered" role="document">
-                        <div className="modal-content bg-dark">
+                <div className="head__modal modal fade" id="head__modal" tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" data-bs-theme="dark">
+                    <div className="modal-dialog modal-lg modal-dialog-centered" role="document">
+                        <div className="modal-content">
                             <div className="modal-header">
                                 <h5 className="modal-title" id="modalLabel">Mon profil GitHub</h5>
                                 <button type="button" className="close btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
-                            <div className="modal-body">
-                                <img src={info.avatar_url} alt="image de profil" />
-                                <ul>
-                                    <li><i class="bi bi-person"></i> <a href={info.html_url} target="_blank">{info.name}</a></li>
-                                    <li><i class="bi bi-geo-alt"></i> <a href={info.location} target="_blank">{info.location}</a></li>
-                                    <li><i class="bi bi-card-text"></i> {info.bio}</li>
-                                    <li><i class="bi bi-box"></i> Repositories : {info.public_repos}</li>
-                                    <li><i class="bi bi-people"></i> Followers : {info.followers}</li>
-                                    <li><i class="bi bi-people"></i> Following : {info.following}</li>
+                            <div className="modal-body row align-items-md-start p-md-4 px-5">
+                                <div className="col-md-6 col-sm-12 d-flex justify-content-center">
+                                    <img className="img-fluid" src={info.avatar_url} alt="image de profil" />
+                                </div>
+                                <ul className="list-group list-group-flush col-md-6">
+                                    <li className="list-group-item"><i className="bi bi-person"> </i><a href={info.html_url} target="_blank">{info.name}</a></li>
+                                    <li className="list-group-item"><i className="bi bi-geo-alt"> </i><a href={info.location} target="_blank">{info.location}</a></li>
+                                    <li className="list-group-item"><i className="bi bi-card-text"> </i>{info.bio}</li>
+                                    <li className="list-group-item"><i className="bi bi-box"> </i>Repositories : {info.public_repos}</li>
+                                    <li className="list-group-item"><i className="bi bi-people"> </i>Followers : {info.followers}</li>
+                                    <li className="list-group-item"><i className="bi bi-people"> </i>Following : {info.following}</li>
                                 </ul>    
                             </div>
                             <div className="modal-footer">
@@ -54,58 +56,75 @@ export default function Home() {
                     </div>
                 </div>
             </header>
-            <section className="info-section">
+            <section className="info-section container width-phone col-md-10 py-5">
+                <div className="row p-3 rounded shadow">
 
-                {/* J'ai séparé "À propos" et "Mes compétences" en 2 <div> pour la mise en page */}
-                <div className="info-section__about">
-                    <h3>À propos</h3>
-                    <img src={officePic} alt="homme travaillant dans son bureau" />
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Ut molestie vulputate nunc. Morbi pellentesque fermentum magna vel congue. 
-                        Sed sagittis non erat id euismod. Aliquam erat volutpat. Proin vel nisl ipsum. 
-                        Curabitur et nisl a diam suscipit pulvinar quis ac magna. Suspendisse gravida 
-                        neque ac elit malesuada, eu blandit sem fringilla.
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Ut molestie vulputate nunc. Morbi pellentesque fermentum magna vel congue. 
-                        Sed sagittis non erat id euismod. Aliquam erat volutpat. Proin vel nisl ipsum. 
-                        Curabitur et nisl a diam suscipit pulvinar quis ac magna. Suspendisse gravida 
-                        neque ac elit malesuada, eu blandit sem fringilla.
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Ut molestie vulputate nunc. Morbi pellentesque fermentum magna vel congue. 
-                        Sed sagittis non erat id euismod. Aliquam erat volutpat. Proin vel nisl ipsum. 
-                        Curabitur et nisl a diam suscipit pulvinar quis ac magna. Suspendisse gravida 
-                        neque ac elit malesuada, eu blandit sem fringilla.
-                    </p>
-                </div>
-                <div className="info-section__skills">
-                    <h3>Mes compétences</h3>
+                    {/* Separate "À propos" & "Mes compétences" into 2 <div> for the layout */}
+                    <div className="info-section__about col-md col-12">
+                        <h3 className="info-title">A propos</h3>
 
-                    {/* Utilisation de Bootstrap pour les jauges */}
+                        <hr className="border-primary border-5 opacity-100 w-100 mx-auto pb-4"></hr>
 
-                    <h4>HTML5 90%</h4>
-                    <div className="progress">
-                        <div className="progress-bar bg-danger" role="progressbar" style={{width: "95%"}} aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                        <img className="mw-100 pb-4" src={officePic} alt="homme travaillant sur son bureau" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Ut molestie vulputate nunc. Morbi pellentesque fermentum magna vel congue. 
+                            Sed sagittis non erat id euismod. Aliquam erat volutpat. Proin vel nisl ipsum. 
+                            Curabitur et nisl a diam suscipit pulvinar quis ac magna. Suspendisse gravida 
+                            neque ac elit malesuada, eu blandit sem fringilla.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Ut molestie vulputate nunc. Morbi pellentesque fermentum magna vel congue. 
+                            Sed sagittis non erat id euismod. Aliquam erat volutpat. Proin vel nisl ipsum. 
+                            Curabitur et nisl a diam suscipit pulvinar quis ac magna. Suspendisse gravida 
+                            neque ac elit malesuada, eu blandit sem fringilla.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Ut molestie vulputate nunc. Morbi pellentesque fermentum magna vel congue. 
+                            Sed sagittis non erat id euismod. Aliquam erat volutpat. Proin vel nisl ipsum. 
+                            Curabitur et nisl a diam suscipit pulvinar quis ac magna. Suspendisse gravida 
+                            neque ac elit malesuada, eu blandit sem fringilla.
+                        </p>
                     </div>
-                    <h4>CSS3 80%</h4>
-                    <div className="progress">
-                        <div className="progress-bar bg-info" role="progressbar" style={{width: "80%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4>JAVASCRIPT 70%</h4>
-                    <div className="progress">
-                        <div className="progress-bar bg-warning" role="progressbar" style={{width: "70%"}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4>PHP 60%</h4>
-                    <div className="progress">
-                        <div className="progress-bar bg-success" role="progressbar" style={{width: "60%"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4>REACT 50%</h4>               
-                    <div className="progress">
-                       <div className="progress-bar" role="progressbar" style={{width: "50%"}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div className="info-section__skills col-md col-12">
+                        <h3 className="info-title">Mes compétences</h3>
+
+                        <hr className="border-primary border-5 opacity-100 w-100 mx-auto pb-4"></hr>
+
+                        {/* Bootstrap ProgressBars */}
+
+                        <h4 className="card-title" id="html-level">HTML5 90%</h4>
+                        <div className="pt-2 pb-4">
+                            <div className="progress">
+                                <div className="progress-bar bg-danger" role="progressbar" aria-labelledby="html-level" style={{width: "90%"}} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <h4 className="card-title" id="css-level">CSS3 80%</h4>
+                        <div className="pt-2 pb-4">
+                            <div className="progress">
+                                <div className="progress-bar bg-info" role="progressbar" aria-labelledby="css-level" style={{width: "80%"}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>  
+                        <h4 className="card-title" id="js-level">JAVASCRIPT 70%</h4>
+                        <div className="pt-2 pb-4">
+                            <div className="progress">
+                                <div className="progress-bar bg-warning" role="progressbar" aria-labelledby="js-level" style={{width: "70%"}} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>  
+                        <h4 className="card-title" id="php-level">PHP 60%</h4>
+                        <div className="pt-2 pb-4">
+                            <div className="progress">
+                                <div className="progress-bar bg-success" role="progressbar" aria-labelledby="php-level" style={{width: "60%"}} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>  
+                        <h4 className="card-title" id="react-level">REACT 50%</h4> 
+                        <div className="pt-2 pb-4">              
+                            <div className="progress">
+                                <div className="progress-bar" role="progressbar" aria-labelledby="react-level" style={{width: "50%"}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>  
                     </div>
                 </div>
             </section>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,5 @@
+import banner from "../assets/img/banner.jpg";
+
 import freshFood from "../assets/img/portfolio/fresh-food.jpg";
 import akira from "../assets/img/portfolio/restaurant-japonais.jpg";
 import bienEtre from "../assets/img/portfolio/espace-bien-etre.jpg";
@@ -9,50 +11,58 @@ export default function Projects() {
     return(
         <main className="text-center">
             <header className="hero-section">
-                <h1 className="hero-section__title">Portfolio</h1>
-                <p className="hero-section__subtitle">Voici quelques-unes de mes réalisations.</p>
+                <img className="hero-section__banner" src={banner} alt="bannière de site" />
 
-                <hr class="border-primary border-5 opacity-100 w-25 mx-auto"></hr>
+                <h1 className="hero-section__title pt-5">Portfolio</h1>
+                <p className="px-md-5 px-2">Voici quelques-unes de mes réalisations.</p>
+
+                <hr className="border-primary border-5 opacity-100 w-25 mx-auto mb-0"></hr>
             </header>
 
-            {/* Cards Bootstrap */}
+            {/* Bootstrap Cards */}
             <section className="project-section container py-5">
                 <div className="project-section__1 row d-flex justify-content-center g-4 py-2">
                     <div className="col-md-4">
-                        <div className="fresh-food card">
+                        <div className="fresh-food card h-100">
                             <img src={freshFood} className="card-img-top" alt="panier de légumes" />
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2 className="card-title">Fresh Food</h2>
                                 <p className="card-text">Site de vente de produits frais en ligne</p>
-                                <a href="#" className="btn btn-primary" rel="nofollow">Voir le site</a>
+                                <a href="#" className="btn btn-primary active btn-lighter mt-auto align-self-center" rel="nofollow">
+                                    Voir le site
+                                </a>
                             </div>
-                            <div class="card-footer text-body-secondary">
+                            <div className="card-footer text-body-secondary">
                                 Site réalisé avec PHP et MySQL
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <div className="akira card">
+                        <div className="akira card h-100">
                             <img src={akira} className="card-img-top" alt="plats japonais" />
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2 className="card-title">Restaurant Akira</h2>
                                 <p className="card-text">Site de vente de produits frais en ligne</p>
-                                <a href="#" className="btn btn-primary" rel="nofollow">Voir le site</a>
+                                <a href="#" className="btn btn-primary active btn-lighter mt-auto align-self-center" rel="nofollow">
+                                    Voir le site
+                                </a>
                             </div>
-                            <div class="card-footer text-body-secondary">
+                            <div className="card-footer text-body-secondary">
                                 Site réalisé avec WordPress
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <div className="bien-etre card">
+                        <div className="bien-etre card h-100">
                             <img src={bienEtre} className="card-img-top" alt="statue de Bouddha sur l'eau" />
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2 className="card-title">Espace bien-être</h2>
                                 <p className="card-text">Site de vente de produits frais en ligne</p>
-                                <a href="#" className="btn btn-primary" rel="nofollow">Voir le site</a>
+                                <a href="#" className="btn btn-primary active btn-lighter mt-auto align-self-center" rel="nofollow">
+                                    Voir le site
+                                </a>
                             </div>
-                            <div class="card-footer text-body-secondary">
+                            <div className="card-footer text-body-secondary">
                                 Site réalisé avec LARAVEL
                             </div>
                         </div>
@@ -60,40 +70,46 @@ export default function Projects() {
                 </div>
                 <div className="project-section__2 row d-flex justify-content-center g-4 py-2">
                     <div className="col-md-4">
-                        <div className="seo card">
+                        <div className="seo card h-100">
                             <img src={seo} className="card-img-top" alt="nuage de mots" />
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2 className="card-title">SEO</h2>
                                 <p className="card-text">Amélioration du référencement d'un site e-commerce</p>
-                                <a href="#" className="btn btn-primary" rel="nofollow">Voir le site</a>
+                                <a href="#" className="btn btn-primary active btn-lighter mt-auto align-self-center" rel="nofollow">
+                                    Voir le site
+                                </a>
                             </div>
-                            <div class="card-footer text-body-secondary">
+                            <div className="card-footer text-body-secondary">
                                 Utilisation des outils SEO
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <div className="create-api card">
+                        <div className="create-api card h-100">
                             <img src={createApi} className="card-img-top" alt="lignes de code" />
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2 className="card-title">Création d'une API</h2>
                                 <p className="card-text">Création d'une API RESTFULL publique</p>
-                                <a href="#" className="btn btn-primary" rel="nofollow">Voir le site</a>
+                                <a href="#" className="btn btn-primary active btn-lighter mt-auto align-self-center" rel="nofollow">
+                                    Voir le site
+                                </a>
                             </div>
-                            <div class="card-footer text-body-secondary">
+                            <div className="card-footer text-body-secondary">
                                 PHP - SYMFONY
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <div className="maquette-figma card">
+                        <div className="maquette-figma card h-100">
                             <img src={maquetteFigma} className="card-img-top" alt="matériel informatique" />
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column">
                                 <h2 className="card-title">Maquette d'un site web</h2>
                                 <p className="card-text">Création du prototype d'un site</p>
-                                <a href="#" className="btn btn-primary" rel="nofollow">Voir le site</a>
+                                <a href="#" className="btn btn-primary active btn-lighter mt-auto align-self-center" rel="nofollow">
+                                    Voir le site
+                                </a>
                             </div>
-                            <div class="card-footer text-body-secondary">
+                            <div className="card-footer text-body-secondary">
                                 Réalisé avec FIGMA
                             </div>
                         </div>
